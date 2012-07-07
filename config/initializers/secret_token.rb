653fd5359b5a798aba7a541b12e24876112b867d
Adrienne Peirce <adrienne.peirce@gmail.com>
2012-07-06 22:22:01 -0500

    redo initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartlapp::Application.config.secret_token = '26a2c076d9e73b75b811abb99efb90a8df06bfbbd1fa072db2dee07992516c64078c9751b9c0f80ed1e1bb19af9adf3ba61a2eb5311580523fa7406a20f0997f'
